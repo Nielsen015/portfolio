45a8086c565738fade3bb2ecc57b493e2a2e4bd6
Moses Nielsen <mosesnielsen68@gmail.com>
2025-02-14 22:32:15 +0300

    Resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -136,16 +136,38 @@ const skills = {
   ]
 };
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "/components/ui/tabs";
 
-import { Tooltip,TooltipContent, TooltipProvider, TooltipTrigger } from "@components/ui/tooltip";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tooltip,TooltipContent, TooltipProvider, TooltipTrigger } from "/components/ui/tooltip";
+import { ScrollArea } from "/components/ui/scroll-area";
 import { motion } from "framer-motion";
 
 
 
 const resume = () => {
-  return <div>Resume resume</div>;
+  return <motion.div initial={{ opacity:0 }} animate={{ 
+    opacity: 1,
+    transition: {
+      delay:2.4,
+      duration: 0.4,
+      ease: "easeIn"
+    },
+   }} className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0">
+   <div className="container mx-auto">
+    <Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
+      <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0">
+        <TabsTrigger value='experience'>Experience</TabsTrigger>
+        <TabsTrigger value='education'>Eduaction</TabsTrigger>
+        <TabsTrigger value='skills'>Skills</TabsTrigger>
+        <TabsTrigger value='about'>About me</TabsTrigger>
+      </TabsList>
+      {/* content */}
+      <div className="min-h-[70vh] w-full" >
+      {/* Experience */}
+      <TabsContent value="experience" className="w-full">Experience</TabsContent>
+      </div>
+    </Tabs>
+   </div></motion.div>;
 };
 
 export default resume;
