289b7aa36339b50814ec30411e57aedc6462e693
Moses Nielsen <mosesnielsen68@gmail.com>
2025-02-18 04:04:19 +0300

    My works pages

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,172 @@
+"use client";
+import {motion} from "framer-motion";
+import React, {useState} from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {BsArrowUpRight, BsGithub} from "react-icons/bs";
+import Link from "next/link";
+import {Tooltip,TooltipProvider, TooltipTrigger, TooltipContent} from "@/components/ui/tooltip";
+import Image from "next/image"; 
+import WorkSliderBtns from "@/components/ui/WorkSliderBtns";
+
+
+const projects = [
+  {
+    num: "01",
+    category: "Web Development",
+    title: "Laravel Ecommerce",
+    description: "A fully functional e-commerce platform built with Laravel, designed to provide a seamless shopping experience. Features robust backend functionality, secure payment integration, and an intuitive user interface.",
+    stack: [
+      { name: "Laravel" },
+      { name: "PHP" },
+      { name: "MySQL" },
+      { name: "Bootstrap" }
+    ],
+    image: "/assets/projects/breather.png",
+    live: "https://breatherleatherke.com/",
+    github: "https://github.com/Nielsen015/BreatherLeather",
+  },
+  {
+    num: "02",
+    category: "Web Development",
+    title: "Business Web App",
+    description: "A dynamic portfolio website developed with React, showcasing the innovative work of Jasiri Labs. Emphasizes clean design, smooth animations, and a user-friendly interface.",
+    stack: [
+      { name: "React" },
+      { name: "JavaScript" },
+      { name: "CSS 3" },
+      { name: "Framer Motion" }
+    ],
+    image: "/assets/projects/jasiri.png",
+    live: "https://www.jasirilabs.tech/",
+    github: "https://github.com/Jasiri-Labs-Tech/jasiri-labs-website-frontend",
+  },
+  {
+    num: "03",
+    category: "Web Development",
+    title: "Rental Web App",
+    description: "A comprehensive system for managing rental properties, offering features like property listings, tenant management, and payment tracking. Built with a focus on usability and efficiency.",
+    stack: [
+      { name: "HTML 5" },
+      { name: "CSS 3" },
+      { name: "JavaScript" },
+      { name: "Bootstrap" }
+    ],
+    image: "/assets/projects/home.png",
+    live: "https://molbs.github.io/RealEstate-Agency/home",
+    github: "https://github.com/Nielsen015/Real-Estate-agency",
+  },
+  {
+    num: "04",
+    category: "Web Development",
+    title: "Ecommerce",
+    description: "An e-commerce solution with an intuitive UI, designed to enhance the online shopping experience. Includes product catalogs, cart functionality, and secure checkout processes.",
+    stack: [
+      { name: "React" },
+      { name: "JavaScript" },
+      { name: "CSS 3" },
+      { name: "Netlify" }
+    ],
+    image: "/assets/projects/eco.png",
+    live: "https://nielsenm.netlify.app/",
+    github: "https://github.com/Nielsen015/shop",
+  },
+  {
+    num: "05",
+    category: "Graphic Design",
+    title: "Poster Designs",
+    description: "A collection of visually striking event poster designs,logos created to capture attention and communicate key information effectively. Reflects my ability to blend creativity with purpose.",
+    stack: [
+      { name: "Adobe Illustrator" },
+      { name: "Photoshop" },
+      { name: "Canva" }
+    ],
+    image: "/assets/projects/Weso_worshipNight.png",
+    live: "https://drive.google.com/drive/folders/1Mp7jgovTowVOw7SleTuqKgbFA5CcQ-cX?usp=sharing",
+github: "https://github.com/Nielsen015",
+  },
+];
+
+
 const Work = () => {
-  return <div>Works Work</div>;
+  const [project, setProject] = useState(projects[0]);
+  const handleSlideChange = (swiper) =>{
+    // get the current slide index
+    const currentIndex = swiper.activeIndex;
+    // update projetc state based on current slide index
+
+    setProject(projects[currentIndex]);
+  }
+  return (<motion.section initial={{ opacity:0 }} animate={{ opacity:1, transition:{delay: 2.4, duration:0.4, ease: "easeIn"} }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+  <div className="container mx-auto">
+  <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+    <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+    <div className="flex flex-col gap-[30px] h-[50%]">
+      {/* outline num */}
+      <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
+      {/* project category */}
+      <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
+      {/* project description */}
+      <p className="text-white/60">{project.description}</p>
+      {/* stack */}
+      <ul className="flex gap-4">
+        {project.stack.map((item,index)=>{
+          return <li key={index} className="text-xl text-accent">{item.name}
+          {/* remove the last comma */}
+          {index !== project.stack.length -1 && ","}
+          </li>;
+        })}
+      </ul>
+      {/* border */}
+      <div className="border border-white/20"></div>
+      {/* buttons */}
+      <div className="flex items-center gap-4">
+      {/* link to project (live) */}
+        <Link href={project.github} target="_blank">
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                <BsArrowUpRight className="text-white text-3xl group-hover:text-accent"></BsArrowUpRight>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Visit Site</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </Link> <Link href={project.live} target="_blank">
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                <BsGithub className="text-white text-3xl group-hover:text-accent"></BsGithub>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Github Repository</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </Link>
+      </div>
+    </div>
+    </div>
+    <div className="w-full xl:w-[50%]">
+        <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+          {projects.map((project,index)=>{
+            return <SwiperSlide key={index} className="w-full">
+              <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+              {/* overlay */}
+                <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                {/* image */}
+                <div className="relative w-full h-full"><Image src={project.image} fill className="object-cover" alt="live image"></Image></div>
+              </div>
+            </SwiperSlide>;
+          })}
+          {/* slider buttons */}
+          <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all" />
+        </Swiper>
+    </div>
+  </div>
+  </div></motion.section>);
 };
 
 export default Work;
